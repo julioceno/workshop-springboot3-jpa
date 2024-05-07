357563f82c5b49e86d984c93077f10a2bdc7f5e4
Júlio Nepomuceno <julioreplay@gmail.com>
2024-05-06 22:39:38 -0300

    first commit

---
+++ src/main/java/com/julionepomcueno/workshopspringboot3jpa/WorkshopSpringboot3JpaApplication.java
@@ -0,0 +1,13 @@
+package com.julionepomcueno.workshopspringboot3jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot3JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot3JpaApplication.class, args);
+	}
+
+}
