2735920faf7538dc777a7a7dd6951312a01e7cce
Júlio Nepomuceno <julioreplay@gmail.com>
2024-05-06 23:13:17 -0300

    [feature]: H2 database, test profile JPA

--- src/main/java/com/julionepomcueno/workshopspringboot3jpa/entities/User.java
+++ src/main/java/com/julionepomcueno/workshopspringboot3jpa/entities/User.java
@@ -1,2 +1,83 @@
-package com.julionepomcueno.workshopspringboot3jpa.entities;public class User {
+package com.julionepomcueno.workshopspringboot3jpa.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String aemail;
+    private String phone;
+    private String password;
+
+    public User() {}
+
+    public User(Long id, String name, String aemail, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.aemail = aemail;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAemail() {
+        return aemail;
+    }
+
+    public void setAemail(String aemail) {
+        this.aemail = aemail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+
 }
--- src/main/java/com/julionepomcueno/workshopspringboot3jpa/resources/UserResource.java
+++ src/main/java/com/julionepomcueno/workshopspringboot3jpa/resources/UserResource.java
@@ -1,2 +1,19 @@
-package com.julionepomcueno.workshopspringboot3jpa.resources;public class UserResource {
+package com.julionepomcueno.workshopspringboot3jpa.resources;
+
+import com.julionepomcueno.workshopspringboot3jpa.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findall() {
+        User u = new User(1L, "maria", "maria@gmail.com", "23", "senha");
+        return ResponseEntity.ok().body(u);
+    }
 }
