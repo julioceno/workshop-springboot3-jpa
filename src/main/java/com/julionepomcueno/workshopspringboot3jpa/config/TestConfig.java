96377d3bf3e1e87f094f8be4883f2f1130af14b9
Júlio Nepomuceno <julioreplay@gmail.com>
2024-05-12 11:05:57 -0300

    [feature]: payment relation one to one

--- src/main/java/com/julionepomcueno/workshopspringboot3jpa/config/TestConfig.java
+++ src/main/java/com/julionepomcueno/workshopspringboot3jpa/config/TestConfig.java
@@ -57,7 +57,7 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(user1, user2));
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, user1);
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, user1);
         Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, user2);
         Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, user1);
 
@@ -69,6 +69,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem orderItem4 = new OrderItem(order3, product5, 2, product5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(orderItem1, orderItem2, orderItem3, orderItem4));
+
+        Payment payment1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), order1);
+        order1.setPayment(payment1);
+
+        orderRepository.save(order1);
 
     }
 }
